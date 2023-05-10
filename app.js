55b4f36aae5b1f0c2cf937615ecdcca1869ffbf5
Natalia Hilarion <natalia.hilarion@outlook.com>
2023-05-09 19:06:49 -0500

    cambiando color de la fuente segun ternario

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ const $btn = document.querySelectorAll(".btn");
 const $value = document.getElementById("value");
 let count = 0;
 
-$btn.forEach(btn =>{
-  btn.addEventListener("click", function(e){
-    let styles = e.currentTarget.classList
-    if(styles.contains("increase")){
+$btn.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    let styles = e.currentTarget.classList;
+    if (styles.contains("increase")) {
       count++;
-  } else if(styles.contains("decrease")){
-    count--
-  } else{
-    count = 0;
-  }
-  $value.innerHTML = count;
-})
-})
+    } else if (styles.contains("decrease")) {
+      count--;
+    } else {
+      count = 0;
+    }
+    count > 0
+    ? ($value.style.color = "green")
+    : count < 0
+      ? ($value.style.color = "red")
+      : $value.style.color = "black"
+
+    $value.innerHTML = count;
+  });
+});
